2e0c5e658a001f5a711ce033817c0e1deb676af5
Lucia Reynoso <lvreynoso@protonmail.com>
2019-10-24 14:54:21 -0700

    Optimize selection sort

--- Code/sorting_iterative.py
+++ Code/sorting_iterative.py
@@ -3,9 +3,9 @@
 
 def is_sorted(items):
     """Return a boolean indicating whether given items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Check that all adjacent items are in order, return early if so
+    Running time: O(n), always
+    Memory usage: O(1) since it's a constant size no matter the input"""
+    # Check that all adjacent items are in order, return early if so
     prev = items[0] if len(items) > 0 else None
     for index in range(1, len(items)):
         num = items[index]
@@ -18,12 +18,12 @@ def is_sorted(items):
 def bubble_sort(items):
     """Sort given items by swapping adjacent items that are out of order, and
     repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Swap adjacent items that are out of order
+    Running time: O(n^2) roughly
+    Memory usage: O(1) since items are merely swapped"""
+    # Repeat until all items are in sorted order
     while not is_sorted(items):
         for index, item in enumerate(items):
+            # Swap adjacent items that are out of order
             if index < len(items) - 1 and item > items[index + 1]:
                 items[index], items[index + 1] = items[index + 1], items[index]
     return items
@@ -32,21 +32,17 @@ def bubble_sort(items):
 def selection_sort(items):
     """Sort given items by finding minimum item, swapping it with first
     unsorted item, and repeating until all items are in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    # TODO: Repeat until all items are in sorted order
-    # TODO: Find minimum item in unsorted items
-    # TODO: Swap it with first unsorted item
-    first_unsorted_index = 0
-    while not is_sorted(items):
+    Running time: O(n^2) cause we make n comparisons n times?
+    Memory usage: O(1) since we're just swapping stuff around"""
+    for unsorted_index in range(len(items)):
         min_index, min_value = None, None
-        for index in range(first_unsorted_index, len(items)):
+        # Find minimum item in unsorted items
+        for index in range(unsorted_index, len(items)):
             if min_value is None or items[index] < min_value:
                 min_index, min_value = index, items[index]
-        items[first_unsorted_index], items[min_index] = min_value, items[first_unsorted_index]
-        first_unsorted_index += 1
+        # Swap it with first unsorted item
+        items[unsorted_index], items[min_index] = min_value, items[unsorted_index]
     return items
-
 
 
 
@@ -59,12 +55,12 @@ def insertion_sort(items):
         # Take first unsorted item
         unsorted = items[unsorted_index]
         # Find the location it belongs in the sorted items
-        target = unsorted_index
-        while unsorted < items[target-1] and target > 0:
-            target -= 1
+        target_index = unsorted_index
+        while unsorted < items[target_index-1] and target_index > 0:
+            target_index -= 1
         # insert the unsorted item and shift the other sorted
         # items down without adjusting the list
         held_item = unsorted
-        for replace_index in range(target, unsorted_index + 1):
+        for replace_index in range(target_index, unsorted_index + 1):
             items[replace_index], held_item = held_item, items[replace_index] 
     return items
