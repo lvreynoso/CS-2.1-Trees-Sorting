c1999b1d0c2de79604b7fb5a64c49a049dc6a532
Lucia Reynoso <lvreynoso@protonmail.com>
2019-12-11 16:20:47 -0800

    Implement counting sort

--- Code/sorting_integer.py
+++ Code/sorting_integer.py
@@ -7,10 +7,23 @@ def counting_sort(numbers):
     TODO: Running time: ??? Why and under what conditions?
     TODO: Memory usage: ??? Why and under what conditions?"""
     # TODO: Find range of given numbers (minimum and maximum integer values)
+    minimum, maximum = min(numbers), max(numbers)
     # TODO: Create list of counts with a slot for each number in input range
+    counts = [0 for _ in range(minimum, maximum + 1)]
     # TODO: Loop over given numbers and increment each number's count
+    for number in numbers:
+        counts[number - minimum] += 1
     # TODO: Loop over counts and append that many numbers into output list
+    # output = []
+    # for number, count in enumerate(counts):
+    #     output.extend([number + minimum for _ in range(count)])
+    # return output
     # FIXME: Improve this to mutate input instead of creating new output list
+    index = 0
+    for number, count in enumerate(counts):
+        numbers[index:index + count] = [number + minimum] * count
+        index += count
+    return numbers
 
 
 def bucket_sort(numbers, num_buckets=10):
@@ -24,3 +37,15 @@ def bucket_sort(numbers, num_buckets=10):
     # TODO: Sort each bucket using any sorting algorithm (recursive or another)
     # TODO: Loop over buckets and append each bucket's numbers into output list
     # FIXME: Improve this to mutate input instead of creating new output list
+
+def test_integer_sort():
+    jumbled = [17, 73, 14, 10, 36, 75, 25, 39, 55, 4, 35, 54, 22, 7, 54, 13, 17, 84, 41, 91]
+    print(f'Sorting {jumbled}')
+    unjumbled = counting_sort(jumbled)
+    print(f'Result: {unjumbled}')
+    is_sorted = unjumbled == sorted(jumbled)
+    print(is_sorted)
+
+
+if __name__ == '__main__':
+    test_integer_sort()
